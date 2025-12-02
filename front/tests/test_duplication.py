b15f619925db460168d723cfa5ce1e1308021772
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-12-02 15:07:38 +0300

    meow

--- front/tests/test_duplication.py
+++ front/tests/test_duplication.py
@@ -35,9 +35,9 @@ class TestDuplicateBasic:
             Location.Network.ConfigPanel.Edge.SUBMIT_BUTTON.selector,
         ).click()
 
-        selenium.wait_for(lambda _: network.edges[0]["data"].get("duplicate") == 30)
+        selenium.wait_for(lambda _: network.edges[0]["data"].get("duplicate_percentage") == "30")
 
-        assert network.edges[0]["data"]["duplicate"] == "30"
+        assert network.edges[0]["data"]["duplicate_percentage"] == "30"
 
 
 class TestDuplicateEmulation:
@@ -133,6 +133,6 @@ class TestDuplicateCopyNetwork:
 
         assert copy_net.url != network.url
 
-        assert copy_net.edges[0]["data"].get("duplicate") == initial_edges[0][
+        assert copy_net.edges[0]["data"].get("duplicate_percentage") == initial_edges[0][
             "data"
-        ].get("duplicate")
+        ].get("duplicate_percentage")
