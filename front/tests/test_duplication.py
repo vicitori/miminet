e724e3ecbe2ff092393885699a664e9dd7f67a63
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-12-02 16:35:16 +0300

    meow

--- front/tests/test_duplication.py
+++ front/tests/test_duplication.py
@@ -42,61 +42,6 @@ class TestDuplicateBasic:
         assert network.edges[0]["data"]["duplicate_percentage"] == "30"
 
 
-class TestDuplicateEmulation:
-    @pytest.fixture(scope="class")
-    def network(self, selenium: MiminetTester):
-        network = MiminetTestNetwork(selenium)
-
-        h1 = network.add_node(NodeType.Host)
-        h2 = network.add_node(NodeType.Host)
-        network.add_edge(h1, h2)
-
-        # config host1 → ping host2
-        cfg = network.open_node_config(h1)
-        cfg.fill_link("10.0.0.1", 24)
-        cfg.add_jobs(
-            1, {Location.Network.ConfigPanel.Host.Job.PING_FIELD.selector: "10.0.0.2"}
-        )
-        cfg.submit()
-
-        cfg2 = network.open_node_config(h2)
-        cfg2.fill_link("10.0.0.2", 24)
-        cfg2.submit()
-
-        yield network
-        network.delete()
-
-    def test_duplicate_changes_packet_count(
-        self, selenium: MiminetTester, network: MiminetTestNetwork
-    ):
-        edge = network.edges[0]
-        network.open_edge_config(edge)
-
-        # Сначала запускаем эмуляцию, чтобы получить базовое количество пакетов
-        packets_before = network.run_emulation()
-
-        dup_elem = selenium.find_element(
-            By.CSS_SELECTOR, Location.Network.ConfigPanel.Edge.DUPLICATE_FIELD.selector
-        )
-        # Очистим поле и установим 100% дублирование
-        dup_elem.clear()
-        dup_elem.send_keys("100")  # 100% duplication
-
-        selenium.find_element(
-            By.CSS_SELECTOR, Location.Network.ConfigPanel.Edge.SUBMIT_BUTTON.selector
-        ).click()
-
-        # Ждём, пока конфигурация применится в модели сети
-        selenium.wait_for(
-            lambda _: network.edges[0]["data"].get("duplicate_percentage") == "100"
-        )
-
-        packets_after = network.run_emulation()
-
-        # Ожидаем детерминированного увеличения числа пакетов
-        assert len(packets_after) > len(packets_before), "Duplicate did not increase packet count"
-
-
 class TestDuplicateCopyNetwork:
     @pytest.fixture(scope="class")
     def network(self, selenium: MiminetTester):
